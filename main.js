5723a0900b28c448575c304d63842b9a80708d55
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-03-25 10:47:34 +0700

    update

--- main.js
+++ main.js
@@ -3,8 +3,16 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
+            url:'https://www.vuemastery.com/',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            inventory:100,
+            onSale:true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                { id: 2234, color: 'green' , sizes: 's m l xl' },
+                { id: 2235, color: 'blue' , sizes: 's m l xl'},
+                { }
+              ]
         }
     }
 })
