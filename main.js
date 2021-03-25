6c02d87ebe76eeb2a4ebe3ffd2a4c9701ade8940
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-03-25 10:33:31 +0700

    update

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
+            product: 'Socks',
+            image:'./assets/images/socks_green.jpg',
+            url:'https://www.vuemastery.com/',
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            inventory:100,
+            onSale: true,
+            description:'Hi nice to meet you'
         }
     }
 })
