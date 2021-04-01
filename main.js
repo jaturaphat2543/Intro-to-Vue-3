24ad8153396e001013e29585cdc442c0370f9c7d
jaturaphat2543 <6206021611095@fitm.kmutnb.ac.th>
2021-04-01 09:09:49 +0700

    update

--- main.js
+++ main.js
@@ -10,6 +10,26 @@ const app = Vue.createApp({
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
+
+           
         }
+    },
+    methods:{
+        addToCart(){
+            this.cart += 1
+        },
+
+        removeToCart(){
+
+            if(this.cart >=1){
+                this.cart -=1
+            }
+        },
+        
+        updateImage(variantImage){
+                this.image=variantImage
+            }
     }
+    
+
 })
